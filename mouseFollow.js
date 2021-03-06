1ced1b60b9e92e20d0c174032e56e795a04289e9
orjon <github@orjon.com>
2021-03-06 20:58:37 +0000

    refactored

--- mouseFollow.js
+++ mouseFollow.js
@@ -1,9 +1,18 @@
-document.addEventListener('mousemove', function (event) {
-  const mouseX = event.pageX;
-  const mouseY = event.pageY;
+const ball = document.getElementById('ball');
 
-  const ball = document.getElementById('ball');
+let mouseX = 0;
+let mouseY = 0;
 
+function animate() {
   ball.style.left = mouseX + 'px';
   ball.style.top = mouseY + 'px';
+
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+document.addEventListener('mousemove', function (event) {
+  mouseX = event.pageX;
+  mouseY = event.pageY;
 });
